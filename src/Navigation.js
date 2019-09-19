e089298fc8d83e4305e4a628fd5fab8cfbd92495
nataliegarate <natalieegarate@hotmail.com>
2019-09-18 23:33:13 -0400

    added nav-bar and inital about content

---
+++ src/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Navigation (){
+  return (
+    <div className='nav'>
+      <ul className='nav-list'>
+        <li>About</li>
+        <li>Resume</li>
+        <li>Projects</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
