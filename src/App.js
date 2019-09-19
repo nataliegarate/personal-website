c6886c5a4856b279614221008e9110985719074d
nataliegarate <natalieegarate@hotmail.com>
2019-09-19 00:55:43 -0400

    added skills section and name as header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navigation from './Navigation'
 import About from './About'
+import Skills from './Skills'
 
 function App() {
   return (
     <div>
       <Navigation/>
       <About/>
+      <Skills/>
     </div>
   );
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import './App.css';
 function Navigation (){
   return (
     <div className='nav'>
+    <div className='nav-header'> <div id='nav-header-text'>Natalie Garate</div></div>
       <ul className='nav-list'>
         <li>About</li>
         <li>Resume</li>
