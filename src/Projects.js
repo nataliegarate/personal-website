e3dc0aaba7f66ebecb9d368717a76b9d00b6c74e
nataliegarate <natalieegarate@hotmail.com>
2019-09-21 23:33:17 -0400

    added to project section

--- src/Projects.js
+++ src/Projects.js
@@ -6,6 +6,32 @@ function Projects() {
       <div className='projects-title'>
         Projects:
       </div>
+      <div className='all-project-items-container'>
+        <div className='project-item'>
+          <div className='single-sub-item'>
+            <img src='https://picsum.photos/id/237/300/300'/>
+          </div>
+          <div className='single-text-item'>
+            Desciption blah blah blah blah blah blah blah
+          </div>
+        </div>
+        <div className='project-item'>
+          <div className='single-sub-item'>
+            <img src='https://picsum.photos/id/237/300/300'/>
+          </div>
+          <div className='single-text-item'>
+            Desciption blah blah blah blah blah blah blah
+          </div>
+        </div>
+        <div className='project-item'>
+          <div className='single-sub-item'>
+            <img src='https://picsum.photos/id/237/300/300'/>
+          </div>
+          <div className='single-text-item'>
+            Desciption blah blah blah blah blah blah blah
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
