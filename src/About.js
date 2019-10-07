5b65c43e8b637a9664f888d788bbf7ba01235954
nataliegarate <natalieegarate@hotmail.com>
2019-10-06 21:46:14 -0400

    fixed typo

--- src/About.js
+++ src/About.js
@@ -6,28 +6,29 @@ function App() {
 
     <div className='about-page'>
 
- <div
-          className='about-item-container-paragraph'>
-                   <img alt= 'Natalie Garate'src='./circle.png' />
-<br/>
-          <div id='about-paragraph-header'>
+      <div
+        className='about-item-container-paragraph'>
+        <img alt='Natalie Garate' src='./circle.png' />
+        <br />
+        <div id='about-paragraph-header'>
           About
           </div>
-          <div id='about-paragraph'>
-            Hello World! I am a fullstack developer living in New York City and a recent graduate of the Grace Hopper Program at Fullstack Academy. Before coding, my background was in solving people problems- I studied psychology and worked in employee engagement at a tech company. I'm really excited to be transitioning into the world of tech.
-            <br/>
-            <br/>
-            <div id='skills'>
-            Skills: Javascript, Node, React, Redux, Express, Sequelize with PostgreSQL
+        <div id='about-paragraph'>
+          Hello World! I am a fullstack developer living in New York City and a recent graduate of the Grace Hopper Program at Fullstack Academy. Before coding, my background was in solving people problems- I studied psychology and worked in employee engagement at a tech company. I'm really excited to be transitioning into the world of software development.
+            <br />
+          <br />
+          <div id='skills'>
+            Skills: Javascript, HTML, CSS, Node, React, Redux, Express, Sequelize, PostgreSQL
             </div>
-      </div>
-
-
         </div>
 
 
-
       </div>
+
+
+
+
+    </div>
   );
 }
 
