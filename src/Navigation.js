b04cf62272c396c446cc337924f6938e29ff3796
nataliegarate <natalieegarate@hotmail.com>
2019-10-09 21:01:43 -0400

    fixed contact issue and adjusted headline

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function Navigation (){
+function Navigation() {
   return (
     <div className='nav'>
-    <div className='nav-header'> <div id='nav-header-text'>Natalie Garate</div></div>
+      <div className='nav-header'>
+        <div id='nav-header-text'>Natalie Garate</div>
+      </div>
       <ul className='nav-list'>
-
         <li> <a href='resume.pdf' target="_blank" rel="noopener noreferrer">Resume </a></li>
-
         <li><a href='#footer-container'>Contact</a></li>
       </ul>
     </div>
