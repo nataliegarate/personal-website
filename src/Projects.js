b9603c32f4a603983a984721e57843e24b19b312
nataliegarate <natalieegarate@hotmail.com>
2020-01-26 13:51:49 -0800

    updated info for gratitude master

--- src/Projects.js
+++ src/Projects.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Projects() {
   let projects = [ 
-    {name: 'Alternate Portfolio', description: 'A portfolio website that display my skills and projects, with a more colorful theme than my default portfolio website', technologies: 'JavaScript, Node, React, React-MDL', imageUrl:"./AlternatePortfolio.png", imageAlt: 'Photo of Alternate Portfolio website', link: 'https://github.com/nataliegarate/alternate_portfolio'},
-    {name: 'Gratitude Master', description: 'A website that encourages users to track what makes them happy by providing an online journal space.', technologies: 'JavaScript, Node, React, Redux, Express, and Sequelize with PostgreSQL', imageUrl:"./GratitudeMaster.png", imageAlt: 'Photo of Gratitude Master website', link: 'https://github.com/nataliegarate/gratitude-tracker'},
-    {name: 'Music Mapper', description: 'A data visualization website that displays a Spotify user’s musical preferences with various colorful graphs.', technologies: 'Javascript, Node, d3, React, Redux, Express, Spotify API, and Musixmatch API', imageUrl: './MusicMapper.png', imageAlt: 'Photo of music website', link: 'https://spotify-music-mapper.herokuapp.com'},
-    {name: 'Best Cupcakes', description:'An E-Commerce that sells delicious cupcakes. It has a store inventory, shopping cart, and checkout feature.', technologies: 'JavaScript, Node, React, Redux, Express, and Sequelize with PostgreSQL', imageUrl:"./BestCupcakes.png", imageAlt: 'photo of cupcake website', link: 'http://cupcake-shop-app.herokuapp.com/'},
-    {name: 'Name That Dog', description: 'A drag and drop matching game for learning dog breeds. I had fun building this during a 4 day stackathon.', technologies: 'Javascript, Node, Vue, and Firebase', imageUrl: './NameThatDog.png', imageAlt: 'Photo of dog website', link: 'http://dogapp.nataliegarate.com/#/'},
+    {id: 'gratitude', name: 'Gratitude Master', description: 'A website that encourages users to track what makes them happy by providing an online journal space.', technologies: 'JavaScript, Node, React, Redux, Express, and Sequelize with PostgreSQL', imageUrl:"./GratitudeMaster.png", imageAlt: 'Photo of Gratitude Master website', link: 'https://github.com/nataliegarate/gratitude-tracker'},
+    {id: 'alternate', name: 'Alternate Portfolio', description: 'An alternate portfolio website that displays my skills and projects.', technologies: 'JavaScript, Node, React, React-MDL', imageUrl:"./AlternatePortfolio.png", imageAlt: 'Photo of Alternate Portfolio website', link: 'https://github.com/nataliegarate/alternate_portfolio'},
+    {id: 'music', name: 'Music Mapper', description: 'A data visualization website that displays a Spotify user’s musical preferences with various colorful graphs.', technologies: 'Javascript, Node, d3, React, Redux, Express, Spotify API, and Musixmatch API', imageUrl: './MusicMapper.png', imageAlt: 'Photo of music website', link: 'https://spotify-music-mapper.herokuapp.com'},
+    {id: 'cupcake', name: 'Best Cupcakes', description:'An E-Commerce that sells delicious cupcakes. It has a store inventory, shopping cart, and checkout feature.', technologies: 'JavaScript, Node, React, Redux, Express, and Sequelize with PostgreSQL', imageUrl:"./BestCupcakes.png", imageAlt: 'photo of cupcake website', link: 'http://cupcake-shop-app.herokuapp.com/'},
+    {id: 'dog', name: 'Name That Dog', description: 'A drag and drop matching game for learning dog breeds. I had fun building this during a 4 day stackathon.', technologies: 'Javascript, Node, Vue, and Firebase', imageUrl: './NameThatDog.png', imageAlt: 'Photo of dog website', link: 'http://dogapp.nataliegarate.com/#/'},
 
  ]
   return (
@@ -16,7 +16,7 @@ function Projects() {
       </div>
       <div className='all-project-items-container'>
        {projects.map(project => (
-          <div className='project-item'>
+          <div id={projects.id} className='project-item' >
            <div className='single-sub-item'>
              <a href= {project.link} target="_blank" rel="noopener noreferrer">
                <img alt={project.imageAlt} src={project.imageUrl} className='project-photo' />
