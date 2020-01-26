c91375fd94db0c6e7ac5bbe6a703b371bf30cb1e
nataliegarate <natalieegarate@hotmail.com>
2020-01-26 14:08:30 -0800

    updated about me paragraph

--- src/About.js
+++ src/About.js
@@ -10,12 +10,19 @@ function App() {
         <div id='about-paragraph-header'>
           About
         </div>
-        <div id='about-paragraph'>
-          Hello World! I am a fullstack developer living in New York City and a recent graduate of the Grace Hopper Program at Fullstack Academy. Before coding, my background was in solving people problems- I studied psychology and worked in employee engagement at a tech company. I'm really excited to be transitioning into the world of software development.
-          <br />
+        <div id='about-paragraph'> Hello World! 
+        Prior to studying a software engineering, I worked at a tech company in office operations.
+While working at that tech company and being immersed in the tech culture of Seattle, I became interested in learning more about how programmers build everything.
+I learned the basics of programming on my own and found I had real passion for it.
+I followed my love of programming and committed to attending a full-time coding Bootcamp, Fullstack Academy. 
+
+<br/>
+Now that I have graduated, I am interested in working alongside others who share my enthusiasm and curiosity for tech.
+
+        <br />
           <br />
           <div id='skills'>
-            Skills: Javascript, HTML, CSS, Node, React, Redux, Express, Sequelize, PostgreSQL
+            Skills: Javascript, HTML, CSS, Node, React, Redux, Express, Sequelize, PostgreSQL, Testing, Heroku
           </div>
         </div>
       </div>
