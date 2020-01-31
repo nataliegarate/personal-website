ba0a402d45cafb44f87ceeb8e61a8610a7637788
nataliegarate <natalieegarate@hotmail.com>
2020-01-30 19:34:39 -0800

    fixed typo in about paragraph

--- src/About.js
+++ src/About.js
@@ -11,7 +11,7 @@ function App() {
           About
         </div>
         <div id='about-paragraph'> Hello World! 
-        Prior to studying a software engineering, I worked at a tech company in office operations.
+        Prior to studying software engineering, I worked at a tech company in office operations.
 While working at that tech company and being immersed in the tech culture of Seattle, I became interested in learning more about how programmers build everything.
 I learned the basics of programming on my own and found I had a real passion for it.
 I followed my love of programming and committed to attending a full-time coding Bootcamp, Fullstack Academy. 
