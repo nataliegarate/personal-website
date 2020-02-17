244b67b4d8fe7192a7d5f533f68e9421da201434
nataliegarate <natalieegarate@hotmail.com>
2020-02-16 16:38:31 -0800

    updated about me per Yasmin suggestions

--- src/About.js
+++ src/About.js
@@ -10,11 +10,8 @@ function App() {
         <div id='about-paragraph-header'>
           About
         </div>
-        <div id='about-paragraph'> Hello World! 
-        Prior to studying software engineering, I worked at a tech company in office operations.
-While working at that tech company and being immersed in the tech culture of Seattle, I became interested in learning more about how programmers build everything.
-I learned the basics of programming on my own and found I had a real passion for it.
-I followed my love of programming and committed to attending a full-time coding Bootcamp, Fullstack Academy. 
+        <div id='about-paragraph'> Hello World! Prior to becoming a Software Engineer, I worked at a tech company in office operations. From that experience I became immersed in the tech culture of Seattle, and became increasingly interested in learning more about how programmers build everything. I learned the basics of programming on my own and found that I had a real passion for it. I followed my love of programming and committed to attending a full-time coding bootcamp, Fullstack Academy.
+Now that I have graduated, I am excited to be apart of a team that shares my enthusiasm and curiosity for tech.
 
 <br/>
 Now that I have graduated, I am interested in working alongside others who share my enthusiasm and curiosity for tech.
@@ -22,7 +19,7 @@ Now that I have graduated, I am interested in working alongside others who share
         <br />
           <br />
           <div id='skills'>
-            Skills: Javascript, HTML, CSS, Node, React, Redux, Express, Sequelize, PostgreSQL, Testing, Heroku
+            Skills: Javascript, HTML, CSS, Node, React, Redux, Express, Sequelize, PostgreSQL, Testing, Heroku, Algorithms, Data Structures
           </div>
         </div>
       </div>
