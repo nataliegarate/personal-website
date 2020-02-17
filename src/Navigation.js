9495e1b3c7899d7c9274d0209f5d80a73a8218e8
nataliegarate <natalieegarate@hotmail.com>
2020-02-16 17:36:14 -0800

    changed greeting heading

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
   return (
     <div className='nav'>
       <div className='nav-header'>
-        <div id='nav-header-text'>Natalie Garate</div>
+        <div id='nav-header-text'>Hi, I'm Natalie.</div>
       </div>
       <ul className='nav-list'>
         <li> <a href='resume.pdf' target="_blank" rel="noopener noreferrer">Resume </a></li>
